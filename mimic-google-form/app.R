ee7dcfe74d9c77c2a2937365b03943b73c96249b
Dean Attali <daattali@gmail.com>
2015-06-14 00:46:26 -0700

    mimic-google-form shiny app for blog post

---
+++ mimic-google-form/app.R
@@ -0,0 +1,178 @@
+library(shiny)
+
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+setwd
+fieldsAll <- c("name", "favourite_pkg", "used_shiny", "r_num_years", "os_type")
+fieldsMandatory <- c("name", "favourite_pkg")
+
+# get current Epoch time
+epochTime <- function() {
+  return(as.integer(Sys.time()))
+}
+
+# get a formatted string of the timestamp (exclude colons as they are invalid
+# characters in Windows filenames)
+humanTime <- function() {
+  format(Sys.time(), "%Y%m%d-%H%M%OS")
+}
+
+# save the results to a file
+saveData <- function(data) {
+  fileName <- sprintf("%s_%s.csv",
+                      humanTime(),
+                      digest::digest(data))
+  
+  write.csv(x = data, file = file.path(responsesDir, fileName),
+            row.names = FALSE, quote = TRUE)
+}
+
+loadData <- function() {
+  files <- list.files(file.path(responsesDir), full.names = TRUE)
+  data <- lapply(files, read.csv, stringsAsFactors = FALSE)
+  data <- dplyr::rbind_all(data)
+  data
+}
+
+responsesDir <- file.path("responses")
+
+appCSS <-
+  ".mandatory_star { color: red; }
+   .shiny-input-container { margin-top: 25px; }
+   #submit_msg { margin-left: 15px; }
+   #error { color: red; }
+   #adminPanel { border: 4px solid #aaa; padding: 0 20px 20px; }"
+
+adminUsers <- c("admin", "prof")
+
+shinyApp(
+  ui = fluidPage(
+    shinyjs::useShinyjs(),
+    shinyjs::inlineCSS(appCSS),
+    titlePanel("Mimicking a Google Form with a Shiny app"),
+    
+    fluidRow(
+      column(6,
+        div(
+          id = "form",
+          
+          textInput("name", labelMandatory("Name"), ""),
+          textInput("favourite_pkg", labelMandatory("Favourite R package")),
+          checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
+          sliderInput("r_num_years", "Number of years using R", 0, 25, 2, ticks = FALSE),
+          selectInput("os_type", "Operating system used most frequently",
+                      c("",  "Windows", "Mac", "Linux")),
+          actionButton("submit", "Submit", class = "btn-primary"),
+          
+          shinyjs::hidden(
+            span(id = "submit_msg", "Submitting..."),
+            div(id = "error",
+                div(br(), tags$b("Error: "), span(id = "error_msg"))
+            )
+          )
+        ),
+
+        shinyjs::hidden(
+          div(
+            id = "thankyou_msg",
+            h3("Thanks, your response was submitted successfully!"),
+            actionLink("submit_another", "Submit another response")
+          )
+        )
+      ),
+      column(6,
+        uiOutput("adminPanelContainer")
+      )
+    )
+  ),
+  server = function(input, output, session) {
+    
+    # Enable the Submit button when all mandatory fields are filled out
+    observe({
+      mandatoryFilled <-
+        vapply(fieldsMandatory,
+               function(x) {
+                 !is.null(input[[x]]) && input[[x]] != ""
+               },
+               logical(1))
+      mandatoryFilled <- all(mandatoryFilled)
+      
+      shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+    })
+    
+    # Gather all the form inputs (and add timestamp)
+    formData <- reactive({
+      data <- sapply(fieldsAll, function(x) input[[x]])
+      data <- c(data, timestamp = epochTime())
+      data <- t(data)
+      data
+    })    
+    
+    # When the Submit button is clicked, submit the response
+    observeEvent(input$submit, {
+      
+      # User-experience stuff
+      shinyjs::disable("submit")
+      shinyjs::show("submit_msg")
+      shinyjs::hide("error")
+      
+      # Save the data (show an error message in case of error)
+      tryCatch({
+        saveData(formData())
+        shinyjs::reset("form")
+        shinyjs::hide("form")
+        shinyjs::show("thankyou_msg")
+      },
+      error = function(err) {
+        shinyjs::text("error_msg", err$message)
+        shinyjs::show(id = "error", anim = TRUE, animType = "fade")
+      },
+      finally = {
+        shinyjs::enable("submit")
+        shinyjs::hide("submit_msg")
+      })
+    })
+    
+    observeEvent(input$submit_another, {
+      shinyjs::show("form")
+      shinyjs::hide("thankyou_msg")
+    })
+    
+    
+    output$adminPanelContainer <- renderUI({
+      if (!isAdmin()) return()
+      
+      div(
+        id = "adminPanel",
+        h2("Previous responses (only visible to admins)"),
+        downloadButton("downloadBtn", "Download responses"), br(), br(),
+        DT::dataTableOutput("responsesTable") 
+      )
+    })
+    
+    isAdmin <- reactive({
+      is.null(session$user) || session$user %in% adminUsers
+    })    
+    
+    # Show the responses in the admin table
+    output$responsesTable <- DT::renderDataTable(
+      loadData(),
+      rownames = FALSE,
+      options = list(searching = FALSE, lengthChange = FALSE)
+    )
+    
+    # Allow user to download responses
+    output$downloadBtn <- downloadHandler(
+      filename = function() { 
+        sprintf("mimic-google-form_%s.csv", humanTime())
+      },
+      content = function(file) {
+        write.csv(loadData(), file, row.names = FALSE)
+      }
+    )    
+  }
+)
